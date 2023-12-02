6d63ab556f8c0d7e46b30fbad25247c9fd3bd45a
Roni Kettunen <roni.kettunen@haahtela.fi>
2023-12-02 14:44:06 +0200

    day1 part1

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,31 @@
+{-#  LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Char (isDigit)
+import Data.Maybe (catMaybes)
+import Control.Monad (guard)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = day1part1
+
+getLines :: FilePath -> IO ([B.ByteString])
+getLines = fmap B.lines . B.readFile
+
+day1part1 :: IO ()
+day1part1 = day1 intsFromString1
+
+day1 :: (B.ByteString -> Maybe Int) -> IO ()
+day1 f = do
+  ls <- getLines "data/day1_1.txt"
+  print $ sum . catMaybes $ map f ls
+
+intsFromString1 :: B.ByteString -> Maybe Int
+intsFromString1 bs =
+  let
+    firstDigit = (B.singleton <$>) . B.find isDigit
+  in
+    do
+      intStr <- firstDigit bs <> firstDigit (B.reverse bs)
+      (int, _) <- B.readInt intStr
+      return $ int
