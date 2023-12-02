cc41826061544f7840b9fc0a8c94eacb9f63d39a
Roni Kettunen <roni.kettunen@haahtela.fi>
2023-12-02 18:48:54 +0200

    day1 part2

--- app/Main.hs
+++ app/Main.hs
@@ -3,29 +3,119 @@ module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Char (isDigit)
-import Data.Maybe (catMaybes)
-import Control.Monad (guard)
+import Data.Maybe (mapMaybe)
+import System.Environment (getArgs)
+import Data.List (find)
 
 main :: IO ()
-main = day1part1
+main = do
+  args <- getArgs
+  runSolution args
 
-getLines :: FilePath -> IO ([B.ByteString])
-getLines = fmap B.lines . B.readFile
+type Input = B.ByteString
+type Output = String
+type Solution = Input -> Output
 
-day1part1 :: IO ()
-day1part1 = day1 intsFromString1
-
-day1 :: (B.ByteString -> Maybe Int) -> IO ()
-day1 f = do
-  ls <- getLines "data/day1_1.txt"
-  print $ sum . catMaybes $ map f ls
-
-intsFromString1 :: B.ByteString -> Maybe Int
-intsFromString1 bs =
+runSolution :: [String] -> IO ()
+runSolution args =
   let
-    firstDigit = (B.singleton <$>) . B.find isDigit
+    argInBS = B.pack $ head args
+    (solution, path) = solutionByArg argInBS
   in
     do
-      intStr <- firstDigit bs <> firstDigit (B.reverse bs)
-      (int, _) <- B.readInt intStr
-      return $ int
+      bs <- B.readFile path
+      print $ solution bs
+
+solutionByArg :: B.ByteString -> (Solution, String)
+solutionByArg "1.1" = (day1part1, "data/day1.txt")
+solutionByArg "1.2" = (day1part2, "data/day1.txt")
+solutionByArg _ = (day1part1, "data/day1.txt")
+
+day1part1 :: Solution
+day1part1 = show . day1 intsFromString1
+
+type Day1Strategy = B.ByteString -> Maybe Int
+
+day1 :: Day1Strategy -> B.ByteString -> Int
+day1 f bs = sum $ mapMaybe f (B.lines bs)
+
+intsFromString1 :: Day1Strategy
+intsFromString1 bs =
+  let
+    findDigit :: B.ByteString -> Maybe Char
+    findDigit s = B.find isDigit s
+
+    firstAndLastDigits :: [Char]
+    firstAndLastDigits = mapMaybe findDigit [bs, B.reverse bs]
+  in
+    do
+      (int, _) <- B.readInt $ B.pack firstAndLastDigits
+      pure int
+
+day1part2 :: Solution
+day1part2 = show . day1 intsFromString2
+
+intsFromString2 :: Day1Strategy
+intsFromString2 bs =
+  let
+    firstAndLastDigits :: [Char]
+    firstAndLastDigits = mapMaybe (\x -> x) [firstDigit bs, lastDigit (B.reverse bs)]
+  in
+    do
+      (int, _) <- B.readInt $ B.pack firstAndLastDigits
+      pure int
+
+headIsDigit :: B.ByteString -> Bool
+headIsDigit = isDigit . B.head
+
+firstDigit :: B.ByteString -> Maybe Char
+firstDigit "" = Nothing
+firstDigit s = if headIsDigit s
+  then Just (B.head s)
+  else case getNumberPrefix s of
+    Just prefix -> prefixesToDigit prefix
+    Nothing -> firstDigit (B.tail s)
+
+lastDigit :: B.ByteString -> Maybe Char
+lastDigit "" = Nothing
+lastDigit s = if headIsDigit s
+  then Just (B.head s)
+  else case getNumberSuffix s of
+    Just suffix -> prefixesToDigit $ B.reverse suffix
+    Nothing -> lastDigit (B.tail s)
+
+getPrefix :: B.ByteString -> [B.ByteString] -> Maybe B.ByteString
+getPrefix s = find (\x -> B.isPrefixOf x s)
+
+getNumberPrefix :: B.ByteString -> Maybe B.ByteString
+getNumberPrefix s = getPrefix s prefixes
+
+getNumberSuffix :: B.ByteString -> Maybe B.ByteString
+getNumberSuffix s = getPrefix s suffixes
+
+prefixes :: [B.ByteString]
+prefixes = [ "one"
+           , "two"
+           , "three"
+           , "four"
+           , "five"
+           , "six"
+           , "seven"
+           , "eight"
+           , "nine"
+           ]
+
+suffixes :: [B.ByteString]
+suffixes = B.reverse <$> prefixes
+
+prefixesToDigit :: B.ByteString -> Maybe Char
+prefixesToDigit "one" = Just '1'
+prefixesToDigit "two" = Just '2'
+prefixesToDigit "three" = Just '3'
+prefixesToDigit "four" = Just '4'
+prefixesToDigit "five" = Just '5'
+prefixesToDigit "six" = Just '6'
+prefixesToDigit "seven" = Just '7'
+prefixesToDigit "eight" = Just '8'
+prefixesToDigit "nine" = Just '9'
+prefixesToDigit _ = Nothing
