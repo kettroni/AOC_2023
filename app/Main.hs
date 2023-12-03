b4b190b04d174296778ac37ad900604e2a484db2
Roni Kettunen <roni.kettunen@haahtela.fi>
2023-12-03 07:32:43 +0200

    day2 part1

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@
 module Main where
 
 import qualified Data.ByteString.Lazy.Char8 as B
-import Data.Char (isDigit)
+import Data.Char (isDigit, isSpace)
 import Data.Maybe (mapMaybe)
 import System.Environment (getArgs)
 import Data.List (find)
+import Control.Monad (guard)
 
 main :: IO ()
 main = do
@@ -29,6 +30,7 @@ runSolution args =
 solutionByArg :: B.ByteString -> (Solution, String)
 solutionByArg "1.1" = (day1part1, "data/day1.txt")
 solutionByArg "1.2" = (day1part2, "data/day1.txt")
+solutionByArg "2.1" = (day2part1, "data/day2.txt")
 solutionByArg _ = (day1part1, "data/day1.txt")
 
 day1part1 :: Solution
@@ -119,3 +121,64 @@ prefixesToDigit "seven" = Just '7'
 prefixesToDigit "eight" = Just '8'
 prefixesToDigit "nine" = Just '9'
 prefixesToDigit _ = Nothing
+
+data Bag = Bag { red :: Int
+               , green :: Int
+               , blue :: Int
+               } deriving Show
+
+instance Semigroup Bag where
+  b1 <> b2 = Bag { red   = maximum $ red   <$> [b1, b2]
+                 , green = maximum $ green <$> [b1, b2]
+                 , blue  = maximum $ blue  <$> [b1, b2]
+                 }
+
+instance Monoid Bag where
+  mempty = Bag { red = 0
+               , green = 0
+               , blue = 0
+               }
+
+data Game = Game { identifier :: Int
+                 , maximumBag :: Bag
+                 } deriving Show
+
+game :: Int -> [Bag] -> Game
+game i reveals = Game { identifier = i
+                      , maximumBag = mconcat reveals
+                      }
+
+withinCapacity :: Bag -> Bag -> Bool
+withinCapacity capBag b =
+  (red capBag) >= (red b)
+  && (green capBag) >= (green b)
+  && (blue capBag) >= (blue b)
+
+day2part1 :: Solution
+day2part1 bs = show $ sum . map (identifier) $ filter (withinCapacity capacityBag . maximumBag) $ parseGame <$> B.lines bs
+  where
+    capacityBag = Bag { red = 12
+                      , green = 13
+                      , blue = 14
+                      }
+
+parseGame :: B.ByteString -> Game
+parseGame bs = game n reveals
+  where
+    (gameIdPart, bagsPart) = B.span (/= ':') bs
+    n = parseId gameIdPart
+    reveals = do
+      reveal <- (map (B.dropWhile (not . isDigit))) <$> ((B.split ',') . B.dropWhile (not . isDigit)) <$> (B.split ';' bagsPart)
+      -- reveal <- (B.split ',' . B.takeWhile) <$> (B.split ';' bagsPart)
+      pure $ Bag { red = parseRevealColor reveal " red"
+                 , green = parseRevealColor reveal " green"
+                 , blue = parseRevealColor reveal " blue"
+                 }
+
+parseId :: B.ByteString -> Int
+parseId gamePart = maybe 0 id $ do
+  (a, _) <- B.readInt $ B.dropWhile (not . isDigit) gamePart
+  pure a
+
+parseRevealColor :: [B.ByteString] -> B.ByteString -> Int
+parseRevealColor reveal color = sum . map fst $ filter ((== color) . snd) $ ((maybe (0, color) id) . B.readInt) <$> reveal
